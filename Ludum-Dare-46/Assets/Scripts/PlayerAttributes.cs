b81af283346fb97318f5e20c9b9c62b6af2768b3
grommithub <44238640+grommithub@users.noreply.github.com>
2020-04-18 15:02:23 +0200

    Added some things to th playerattributes and upgrades

--- Ludum-Dare-46/Assets/Scripts/PlayerAttributes.cs
+++ Ludum-Dare-46/Assets/Scripts/PlayerAttributes.cs
@@ -2,8 +2,31 @@
 
 public class PlayerAttributes : MonoBehaviour
 {
-    internal float MovementSpeed { get; private set; }
+    [Header("Level")]
+    private int _level;
+
+    [Header("PlantMeter")]
+    [SerializeField] internal float plantMeterStart = default, plantMeterMax = default;
+    internal float plantMeterCurrent;
+    internal float MovementSpeed { get; private set; } = 1f;
     internal BulletType BulletType { get; private set; }
+
+    private UpgradeManager _upgradeManager;
+
+
+    private void OnEnable()
+    {
+        if (_upgradeManager == null) _upgradeManager = GetComponent<UpgradeManager>();
+
+        UpgradeManager.OnLevelUp += ApplyUpgrade;
+
+        plantMeterCurrent = plantMeterStart;
+        
+    }
+    private void OnDisable()
+    {
+        UpgradeManager.OnLevelUp -= ApplyUpgrade;
+    }
 
     private void ApplyUpgrade(Upgrade upgrade)
     {
@@ -16,6 +39,16 @@ public class PlayerAttributes : MonoBehaviour
             case UpgradeType.BulletType:
                 BulletType = upgrade.BulletTypeValue;
                 break;
+        }
+    }
+
+    internal void AddToPlantMeter(float plantEnergy)
+    {
+        plantMeterCurrent +=  plantEnergy;
+        if(plantMeterCurrent >= plantMeterMax)
+        {
+            plantMeterCurrent = plantMeterStart + (plantMeterCurrent - plantMeterMax);
+            _upgradeManager.LevelUp();
         }
     }
 }
