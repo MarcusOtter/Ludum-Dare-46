5d8535accaed7cc1a1b0a3bf0ac1bf5d1620d566
Marcus Otterström <marcus.otterstrom@gmail.com>
2020-04-19 15:51:13 +0200

    Add rain and health bars to plants

---
+++ Ludum-Dare-46/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _backgroundRenderer;
+    [SerializeField] private SpriteRenderer _fillRenderer;
+
+    [SerializeField] private float _healthPerUnit = 10f;
+
+    private float _maxHealth;
+    private float _fillAmount;
+    private bool _visible = true;
+
+    internal void SetVisibility(bool visible)
+    {
+        _visible = visible;
+        Render();
+    }
+
+    internal void SetHealth(float newHealth, float maxHealth)
+    {
+        _fillAmount = Mathf.Clamp01(newHealth / maxHealth);
+        _maxHealth = maxHealth;
+        Render();
+    }
+
+    private void Render()
+    {
+        _backgroundRenderer.enabled = _visible;
+        _fillRenderer.enabled = _visible;
+
+        if (!_visible) { return; }
+
+        var backgroundScaleX = _maxHealth / _healthPerUnit;
+        _backgroundRenderer.transform.localScale = _backgroundRenderer.transform.localScale.With(x: backgroundScaleX);
+
+        var fillScaleX = _fillAmount * backgroundScaleX;
+        var newFillPositionX = (backgroundScaleX - fillScaleX) / -2f;
+
+        _fillRenderer.transform.localScale = _fillRenderer.transform.localScale.With(x: fillScaleX);
+        _fillRenderer.transform.localPosition = _fillRenderer.transform.localPosition.With(x: newFillPositionX);
+    }
+}
---
+++ Ludum-Dare-46/Assets/Scripts/WaterArea.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class WaterArea : MonoBehaviour
+{
+    [SerializeField] private float _waterPerSecond;
+
+    private List<IWaterable> _waterablesInWaterArea = new List<IWaterable>();
+
+    private void Update()
+    {
+        foreach(var waterable in _waterablesInWaterArea)
+        {
+            if (waterable == null)
+            {
+                _waterablesInWaterArea.Remove(waterable);
+                continue;
+            }
+
+            waterable.Water(_waterPerSecond * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var waterable = collider.GetComponent<IWaterable>();
+        if (waterable == null) { return; }
+
+        _waterablesInWaterArea.Add(waterable);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var waterable = collider.GetComponent<IWaterable>();
+        if (waterable == null) { return; }
+
+        if (_waterablesInWaterArea.Contains(waterable))
+        {
+            _waterablesInWaterArea.Remove(waterable);
+        }
+    }
+}
