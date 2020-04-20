76fbb8c7a4b61a62e8872ffcd22760ba4db98626
grommithub <44238640+grommithub@users.noreply.github.com>
2020-04-20 23:13:51 +0200

    Add seeds to everything and everything drops seeds etc

--- Ludum-Dare-46/Assets/Scripts/PickUpItems.cs
+++ Ludum-Dare-46/Assets/Scripts/PickUpItems.cs
@@ -12,10 +12,10 @@ public class PickUpItems : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("Hi");
         var seed = collision.transform.GetComponent<Seed>();
         if (seed != null)
         {
+            print($"Picked up{seed.PlantToGrowPrefab.PlantType}");
             inventory?.PickUp(seed, 1);
             Destroy(seed.gameObject);
         }
--- Ludum-Dare-46/Assets/Scripts/SeedDrop.cs
+++ Ludum-Dare-46/Assets/Scripts/SeedDrop.cs
@@ -5,9 +5,83 @@ using UnityEngine;
 public class SeedDrop : MonoBehaviour
 {
     [SerializeField] private Seed[] _seedDrops;
+    private enum Dropper
+    {
+        Enemy,
+        Plant,
+        None
+    }
+
+    private Dropper dropper = Dropper.None;
+
+
+    private Enemy enemyComponent;
+    private Plant plantComponent;
+    private void Awake()
+    {
+        enemyComponent = GetComponent<Enemy>();
+        if (enemyComponent != null)
+        {
+            dropper = Dropper.Enemy;
+        }
+        else
+        {
+            plantComponent = GetComponent<Plant>();
+            if (plantComponent != null)
+                dropper = Dropper.Plant;
+        }
+    }
+
+    private void OnEnable()
+    {
+        switch(dropper)
+        {
+            case Dropper.Plant:
+                plantComponent.OnGrowthStageChanged += DropOnPlantGrowUp;
+                break;
+            case Dropper.Enemy:
+                enemyComponent.OnStateChanged += DropOnEnemyDeath;
+                break;
+            default:
+                Debug.LogError($"You've attached a seed dropper to something which can't drop a seed: \n{name}");
+                break;
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        switch (dropper)
+        {
+            case Dropper.Plant:
+                plantComponent.OnGrowthStageChanged -= DropOnPlantGrowUp;
+                break;
+            case Dropper.Enemy:
+                enemyComponent.OnStateChanged -= DropOnEnemyDeath;
+                break;
+        }
+
+    }
+
+    public void DropOnEnemyDeath(EnemyState floob, EnemyState newState)
+    {
+        if (newState == EnemyState.Dead)
+        {
+            TryDropSeed();
+        }
+    }
+
+    public void DropOnPlantGrowUp(GrowthStage floob, GrowthStage newState)
+    {
+        if(newState == GrowthStage.FullyGrown)
+        {
+            TryDropSeed();
+        }
+    }
 
     public void TryDropSeed()
     {
+        print("drop");
         for(int i = 0; i < _seedDrops.Length; i++)
         {
             int r = Random.Range(0, 100);
