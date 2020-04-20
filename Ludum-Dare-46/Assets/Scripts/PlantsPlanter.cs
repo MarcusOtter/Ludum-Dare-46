01bee3446b0ffef6605ac9fb3b599ff842df37e5
Marcus Otterström <marcus.otterstrom@gmail.com>
2020-04-21 01:01:34 +0200

    Plant planter

---
+++ Ludum-Dare-46/Assets/Scripts/PlantsPlanter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlantsPlanter : MonoBehaviour
+{
+    [SerializeField] private PlayerInventory _playerInventory;
+    [SerializeField] private KeyCode _plantKey = KeyCode.Space;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_plantKey))
+        {
+            var slot = _playerInventory.CurrentSelectedSlot;
+
+            if (slot == null) { return; }
+            if (slot.Amount <= 0) { return; }
+
+            var seed = slot.Seed;
+
+            _playerInventory.PickUp(seed, -1);
+            Instantiate(seed.PlantToGrowPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
